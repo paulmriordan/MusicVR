0a4c8912ac86ce7178972041c9d8a43a2e28be7b
Paul Riordan <paulmriordan@hotmail.com>
2017-03-19 21:21:39 +0000

    Tidy up and add bloom

--- Assets/Scripts/WallButton.cs
+++ Assets/Scripts/WallButton.cs
@@ -11,7 +11,6 @@ public class WallButton : MonoBehaviour
 	public Vector3 ScaleMouseDown = new Vector3(1.2f,1.2f,0.6f);
 	public Vector3 ScaleUnselected = new Vector3(1.0f,1.0f,1.0f);
 
-	private Animator m_animator;
 	private MeshRenderer m_meshRenderer;
 	private bool m_selected;
 	public bool MouseDown {get; private set;}
@@ -24,15 +23,12 @@ public class WallButton : MonoBehaviour
 
 	void Awake()
 	{
-		m_animator = GetComponent<Animator>();
 		m_meshRenderer = GetComponent<MeshRenderer>();
 	}
 
 	public void SetSelected(bool selected)
 	{
 		m_selected = selected;
-		m_animator.SetBool("MouseDown", false);
-		m_animator.SetBool("Selected", m_selected);
 		m_meshRenderer.material = m_selected ? SelectedMaterial : UnselectedMaterial;
 	}	
 
@@ -41,7 +37,6 @@ public class WallButton : MonoBehaviour
 		MouseDown = true;
 		s_inputSelectType = m_selected ? E_InputSelectType.unselecting : E_InputSelectType.selecting;
 		SetSelected(s_inputSelectType == E_InputSelectType.selecting);
-		m_animator.SetBool("MouseDown", true);
 	}
 
 	public void OnMouseEnter()
@@ -52,7 +47,6 @@ public class WallButton : MonoBehaviour
 				s_inputSelectType = m_selected ? E_InputSelectType.unselecting : E_InputSelectType.selecting;
 			MouseDown = true;
 			SetSelected(s_inputSelectType == E_InputSelectType.selecting);
-			m_animator.SetBool("MouseDown", true);
 			s_lastHitButton = this;
 		}
 	}
@@ -60,7 +54,6 @@ public class WallButton : MonoBehaviour
 	public void OnMouseExit()
 	{
 		MouseDown = false;
-		m_animator.SetBool("MouseDown", false);
 	}
 
 	public void OnMouseUp()
