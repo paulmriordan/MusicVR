12268dcfcd5f2e49f7964cc2aded83a8f3992529
Paul Riordan <paulmriordan@hotmail.com>
2018-01-21 16:26:11 +0000

    Adding basic UI to left hand

---
+++ Assets/Scripts/Utils/DisableChildrenWhenTargetActive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableChildrenWhenTargetActive : MonoBehaviour {
+
+    public List<GameObject> Targets;
+    
+    private void Update()
+    {
+        bool anyActive = AnyActive();
+        foreach (Transform t in transform)
+            t.gameObject.SetActive(!anyActive);
+    }
+
+    bool AnyActive()
+    {
+        foreach (var t in Targets)
+            if (t.gameObject.activeSelf)
+                return true;
+
+        return false;
+    }
+}
